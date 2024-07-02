93904b2190beecdac407ccc75ad5c7b7ec597804
Urvesh0069 <urveshghodadara82@gmail.com>
2024-07-02 17:23:50 +0530

    Add files via upload

---
+++ ch-7/lw_7_3_1.c
@@ -0,0 +1,468 @@
+#include<stdio.h>
+#define p printf
+#define s scanf
+
+int main()
+{
+    int a, b, c, d, e, f, g;
+    p("Enter a numbers: ");
+    s("%d", &a);
+    p("Enter b numbers: ");
+    s("%d", &b);
+    p("Enter c numbers: ");
+    s("%d", &c);
+    p("Enter d numbers: ");
+    s("%d", &d);
+    p("Enter e numbers: ");
+    s("%d", &e);
+    p("Enter f numbers: ");
+    s("%d", &f);
+    p("Enter g numbers: ");
+    s("%d", &g);
+
+    if(a > b)
+    {
+        if(a > c)
+        {
+            if(a > d)
+            {
+                if(a > e)
+                {
+                    if(a > f)
+                    {
+                        if(a > g)
+                        {
+                            p("largest number is a ");
+                        }
+                        else
+                        {
+                            p("largest number is g ");
+                        }
+                    }
+                    else
+                    {   if(f > g)
+                        {
+                            p("largest number is f ");
+                        }
+                        else
+                        {
+                            p("largest number is g ");
+                        }
+
+                    }
+                }
+                else
+                {
+                    if(e > f)
+                    {
+                        if(e > g)
+                        {
+                            p("largest number is e");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if(d > e)
+                {
+                    if(d > f)
+                    {
+                        if(d > g)
+                        {
+                            p("largest number is d");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+                else
+                {
+                     if(e > f)
+                    {
+                        if(e > g)
+                        {
+                            p("largest number is e");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            if(c > d)
+            {
+                if(c < e)
+                {
+                    if(c > f)
+                    {
+                        if(c > g)
+                        {
+                            p("largest number is c");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+                else
+                {
+                     if(e > f)
+                    {
+                        if(e > g)
+                        {
+                            p("largest number is e");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if(d > e)
+                {
+                    if(d > f)
+                    {
+                        if(d > g)
+                        {
+                            p("largest number is d");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+                else
+                {
+                     if(e > f)
+                    {
+                        if(e > g)
+                        {
+                            p("largest number is e");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        if(b > c)
+        {
+            if(b > d)
+            {
+                if(b > e)
+                {
+                    if(b > f)
+                    {
+                        if(b > g)
+                        {
+                            p("largest number is b");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+                else
+                {
+                     if(e > f)
+                    {
+                        if(e > g)
+                        {
+                            p("largest number is e");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if(d > e)
+                {
+                    if(d > f)
+                    {
+                        if(d > g)
+                        {
+                            p("largest number is d");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+                else
+                {
+                    if(e > f)
+                    {
+                        if(e > g)
+                        {
+                            p("largest number is e");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            if(c > d)
+            {
+                if(c > e)
+                {
+                    if(c > f)
+                    {
+                        if(c > g)
+                        {
+                            p("largest number is c");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+                else
+                {
+                    if(e > f)
+                    {
+                        if(e > g)
+                        {
+                            p("largest number is e");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if(d > e)
+                {
+                    if(d > f)
+                    {
+                        if(d > g)
+                        {
+                            p("largest number is d");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+                else
+                {
+                    if(e > f)
+                    {
+                        if(e > g)
+                        {
+                            p("largest number is e");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                    else
+                    {
+                        if(f > g)
+                        {
+                            p("largest number is f");
+                        }
+                        else
+                        {
+                            p("largest number is g");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+}
