15626299457dc7fac6e866565efaf01bbcffed0c
Urvesh0069 <urveshghodadara82@gmail.com>
2024-07-02 13:09:13 +0530

    Add files via upload

---
+++ ch-7/lw_7_2_3.c
@@ -0,0 +1,270 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+
+    printf("a add number :");
+    scanf("%d",&a);
+     printf("b add number :");
+    scanf("%d",&b);
+     printf("c add number :");
+    scanf("%d",&c);
+     printf("d add number :");
+    scanf("%d",&d);
+     printf("e add number :");
+    scanf("%d",&e);
+     printf("f add number :");
+    scanf("%d",&f);
+
+    if(a > b)
+    {   if(a > c)
+        {   if(a > d)
+            {   if(a > e)
+                {   if(a > f)
+                    {
+                        printf("a is big number ");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+
+                }
+                else
+                {   if(e > f)
+                    {
+                        printf("e is big number");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+
+                }
+
+            }
+            else
+            {   if(d > e)
+                {   if (d > f)
+                    {
+                        printf("d is big number");
+                    }
+                    else
+                    {
+                        printf("f id big number");
+                    }  
+
+                }
+                else
+                {   if(e > f)
+                    {
+                        printf("e is big number");
+                    }
+                    else
+                    {
+                        printf("f id big number");
+                    }
+
+                }
+
+
+            }
+
+        }
+        else
+        {   if(c > d)
+            {   if(c > e)
+                {   if(c > f)
+                    {
+                        printf("c is big number");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+
+                }
+                else
+                {   if(e > f)
+                    {
+                        printf("e is big number");
+                    }
+                    else
+                    {
+                        printf("f id big number");
+                    }
+
+                }
+
+            }
+            else
+            {   if(d > e)
+                {   if(d > f)
+                    {
+                        printf("d is big number");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+
+                }
+                else
+                {   if(e > f)
+                    {
+                        printf("e is big number");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+    else
+    {
+        if(b > c)
+        {   
+            if(b > d)
+            {
+                if(b > e)
+                {
+                    if(b > f)
+                    {
+                        printf("b is big number");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+                }
+                else
+                {   if(e > f)
+                    {
+                        printf("e is big number");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+
+                }
+            }
+            else
+            {
+                if(d > e)
+                {
+                    if(d > f)
+                    {
+                        printf("d is big number");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+                    
+                }
+                else
+                {
+                     if(e > f)
+                    {
+                        printf("e is big number");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+                }
+            }
+        }
+        else
+        {  
+             if(c > d)
+             {
+                if(c > e)
+                {
+                    if(c > f)
+                    {
+                        printf("c is big number");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+                }
+                else
+                {
+                    if(e > f)
+                    {
+                        printf("e is big number");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+                }
+             }
+             else
+             {
+                if(d > e)
+                {
+                    if(d > f)
+                    {
+                        printf("d is big number");
+                    }
+                    else
+                    {
+                        printf("f id big number");
+                    }
+                }
+                else
+                {
+                     if(e > f)
+                    {
+                        printf("e is big number");
+                    }
+                    else
+                    {
+                        printf("f is big number");
+                    }
+                }
+             }
+        }
+    }
+
+
+}
+
+   
+            
+
+            
+        
+        
+
+        
+              
+
+            
+            
+            
+            
+            
+            
+            
+                
+
+                
+                
+                
+
+                
+
+            
+
+  
